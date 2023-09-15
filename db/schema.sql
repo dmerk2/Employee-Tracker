52d5ddd1d78d6fb4c91bf137cde1cdc14b7b6c86
Dan Merkin <dan.merkin@gmail.com>
2023-09-15 11:16:09 -0400

    Adding comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,18 @@
+-- Drop the database employees_db if it already exists
 DROP DATABASE IF EXISTS employees_db;
+-- Create a new database name employees_db
 CREATE DATABASE employees_db;
 
+-- Use the employees_db for the following actions
 USE employees_db;
 
+-- Create a table name department with 2 rows
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name VARCHAR(30) NOT NULL
 );
 
+-- Create a table named role with 4 rows
 CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
@@ -18,6 +23,7 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
+-- Create a table name employee with 5 rows
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,18 @@
+-- Seed the department table with the name of the departments
 INSERT INTO department(name)
 VALUES ("Management"),
        ("Sales"),
        ("Tech"),
        ("Customer Service");
 
+-- Seed the role table
 INSERT INTO role(title, salary, department_id)
 VALUES ("Manager", 125233, 1),
        ("Engineer", 100824, 2),
        ("Technician", 90250, 3),
        ("Sales", 805000, 4);
 
+-- Seed the employee table
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ("John", "Smith", 1, 1),
        ("Peter", "Jones", 2, 2),
